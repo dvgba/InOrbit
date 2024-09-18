06ba2a66af19b174d7ea1e92e74d510e378dbaec
dvgba <diegovianagomes01@gmail.com>
2024-09-18 16:15:19 -0300

    POSTGRE

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'drizzle-kit'
 import { env } from './src/env'
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './.migrations',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+    schema: './src/db/schema.ts',
+    out: './.migrations',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: env.POSTGRES_URL,
+    },
 })
